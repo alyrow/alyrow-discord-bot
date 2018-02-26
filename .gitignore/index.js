99fcc1f00c9ab3c9ec039890544f64d3440ff69e
alyrow <36855289+alyrow@users.noreply.github.com>
2018-02-26 17:55:55 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,13 +8,34 @@ client.on('ready', () => {
 
 client.on('message', message => {
   if (message.content === 'ping') {
-    const emoji = guild.emojis.first();
-msg.reply(`pong ${emoji}`);
+msg.reply(`pong`);
   }
   if (message.content === 'avatar') {
     // Send the user's avatar URL
     message.reply(message.author.avatarURL);
   }
+  
+  
+  /*
+  // Create a new emoji from a url
+guild.createEmoji('https://i.imgur.com/w3duR07.png', 'rip')
+  .then(emoji => console.log(`Created new emoji with name ${emoji.name}!`))
+  .catch(console.error);
+  
+  
+  guild.createRole({
+  name: 'Super Cool People',
+  color: 'BLUE',
+})
+  .then(role => console.log(`Created role ${role}`))
+  .catch(console.error)
+  
+  
+  */
+  
+  
+  
+  
 });
 
 
