c7f15df10f6fcd5bfb33714a83e5963ba18dd050
alyrow <36855289+alyrow@users.noreply.github.com>
2018-02-28 17:48:40 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,6 +1,17 @@
-const Discord = require('discord.js');
+//const Discord = require('discord.js');
+//const client = new Discord.Client();
 
-const client = new Discord.Client();
+const Commando : require('discord.js-commando');
+const client = new Commando.Client();
+
+
+client.registry.registerGroup('test', 'Test')
+client.registry.registerCommandsIn(__dirname + "/commands");
+client.registry.registryDefaults();
+
+
+
+
 
 client.on('ready', () => {
   console.log('I am ready!');
@@ -8,49 +19,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
  
-  
-  if (message.content === 'åhelp') {
-    // Send the user's avatar URL
-    message.reply('COMMANDE :');
-    message.reply('avatar');
-    message.reply('emoji url nom');
-  }
-  
-  if (message.content === 'avatar') {
-    // Send the user's avatar URL
-    message.reply(message.author.avatarURL);
-  }
-  
-  if (message.content.startsWith('emoji')) {
-    // On récupère le premier channel audio du serveur
-    message.reply('breuh');
-    // il faudrait utiliser une expression régulière pour valider le lien youtube
-    let args = message.content.split(' ')
-   
-       // let stream = YoutubeStream(args[1])
-     guild.createEmoji(args[1], args[2])
-  .then(emoji => console.log(`Created new emoji with name ${emoji.name}!`))
-  .catch(console.error);
-  }
-
-  /*
-  // Create a new emoji from a url
-
-  
-  
-  guild.createRole({
-  name: 'Super Cool People',
-  color: 'BLUE',
-})
-  .then(role => console.log(`Created role ${role}`))
-  .catch(console.error)
-  
-  
-  */
-  
-  
-  
-  
+ 
 });
 
 
