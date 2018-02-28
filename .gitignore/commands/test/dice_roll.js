55585daa096c321071836c82e6847488e15b3a8d
alyrow <36855289+alyrow@users.noreply.github.com>
2018-02-28 17:56:21 +0100

    Update dice_roll.js

--- .gitignore/commands/test/dice_roll.js
+++ .gitignore/commands/test/dice_roll.js
@@ -1,1 +1,28 @@
+const Commando = require('discord.js-commando');
 
+
+
+class DiceRollCommand extends Commando.Command {
+
+
+}
+
+
+module.exports = DiceRollCommand;
+
+constructor(client) {
+
+      super(client, {
+        name: 'roll',
+        group: 'test',
+        memberName: 'roll',
+        description: 'Loterie'
+
+    });
+}
+
+async run(message, args){
+var roll = Math.floor(Math.random() * 6) + 1;
+  message.reply("Vous êtes tombé sur un" + roll);
+
+}
