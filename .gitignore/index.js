180375a0d983c9bc1eb12868efc8065f0739a2f0
alyrow <36855289+alyrow@users.noreply.github.com>
2018-03-03 13:30:01 +0100

    Ah

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
 
-var isReady = true;
 
 bot.on('ready', function () {
   console.log("Je suis connecté !")
@@ -10,47 +9,35 @@ bot.on('ready', function () {
 
 bot.on('message', message => {
 
-  if (!message.guild) return;
+  if (message.content === 'how to embed') {
 
-if (message.content === '/join') {
-// Only try to join the sender's voice channel if they are in one themselves
-if (message.member.voiceChannel) {
-const connection = await message.member.voiceChannel.join();
-} else {
-message.reply('Rejoins un chanel vocal !');
+// We can create embeds using the MessageEmbed constructor
+
+// Read more about all that you can do with the constructor
+
+// over at https://discord.js.org/#/docs/main/master/class/MessageEmbed
+
+const embed = new MessageEmbed()
+
+// Set the title of the field
+
+.setTitle('A slick little embed')
+
+// Set the color of the embed
+
+.setColor(0xFF0000)
+
+// Set the main content of the embed
+
+.setDescription('Hello, this is a slick embed!');
+
+// Send the embed to the same channel as the message
+
+message.channel.send(embed);
+
 }
-}
 
 
-  
-  
-  if (message.content.startsWith('!play')) {
-    // On récupère le premier channel audio du serveur
-    let voiceChannel = message.guild.channels
-      .filter(function (channel) { return channel.type === 'voice' })
-      .first()
-    // On récupère les arguments de la commande 
-    // il faudrait utiliser une expression régulière pour valider le lien youtube
-    let args = message.content.split(' ')
-    // On rejoint le channel audio
-    voiceChannel
-      .join()
-      .then(function (connection) {
-        // On démarre un stream à partir de la vidéo youtube
-        let stream = YoutubeStream(args[1])
-        stream.on('error', function () {
-          message.reply("Je n'ai pas réussi à lire cette vidéo :(")
-          connection.disconnect()
-        })
-        // On envoie le stream au channel audio
-        // Il faudrait ici éviter les superpositions (envoie de plusieurs vidéo en même temps)
-        connection
-          .playStream(stream)
-          .on('end', function () {
-            connection.disconnect()
-          })
-      })
-  }
 
 })
   
